fc1fb3e6ff77a5fbf3cb2ae1d1ced4b3dd08b22a
Brock Miller <brock@rolltape.com>
2016-08-04 18:21:45 -0700

    relocation part one...looks pretty good so far.

--- app/components/NavMain.js
+++ app/components/NavMain.js
@@ -1,35 +1,23 @@
 // React
 import React from 'react';
-import { Layout, NavDrawer, Panel, AppBar } from 'react-toolbox';
+import { Layout, Navigation, Panel, AppBar } from 'react-toolbox';
 import { TouchIconButton } from './TouchButton';
 import styles from '../styles/nav_main.scss';
 
 class NavMain extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      navDrawerOpen: false
-    };
-    this.toggleDrawer = this.toggleDrawer.bind(this);
-  }
-
-  toggleDrawer() {
-    this.setState({ navDrawerOpen: !this.state.navDrawerOpen })
-  }
 
   render() {
     return (
       <Layout>
-        <NavDrawer active={this.state.navDrawerOpen} theme={styles}
-          onOverlayClick={this.toggleDrawer}>
-        </NavDrawer>
-
         <Panel theme={styles}>
-          <AppBar fixed flat theme={styles}>
-            <TouchIconButton ripple inverse icon="menu" onClick={this.toggleDrawer}></TouchIconButton>
-          </AppBar>
           {this.props.children}
+          <AppBar flat theme={styles}>
+            <Navigation type='horizontal' >
+              <TouchIconButton icon='trending_up' accent />
+              <TouchIconButton icon='view_headline' accent />
+            </Navigation>
+
+          </AppBar>
         </Panel>
       </Layout>
     );
