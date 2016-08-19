232f4489269e2572bd742d63d47c109d8c8e80c2
Brock Miller <brock@rolltape.com>
2016-08-18 18:58:41 -0700

    intermediate.

--- app/components/NavMain.js
+++ app/components/NavMain.js
@@ -1,22 +1,31 @@
 // React
 import React from 'react';
-import { Layout, Navigation, Panel, AppBar } from 'react-toolbox';
+import { Layout, Navigation, Panel, AppBar, Link } from 'react-toolbox';
 import { TouchIconButton } from './TouchButton';
-import styles from '../styles/nav_main.scss';
+import navMainStyle from '../styles/nav_main.scss';
 
 class NavMain extends React.Component {
+  constructor(props) {
+    super(props)
+
+
+  }
+
+  linkActive(path) {
+    return this.props.location.pathname === path
+  }
 
   render() {
     return (
       <Layout>
-        <Panel theme={styles}>
+        <Panel theme={navMainStyle}>
           {this.props.children}
 
-          <AppBar flat theme={styles}>
-            <Navigation type='horizontal' theme={styles}>
-              <TouchIconButton icon='trending_up' accent />
-              <TouchIconButton icon='view_headline' accent />
-            </Navigation>
+          <AppBar flat theme={navMainStyle}>
+            {/* <Navigation type='horizontal' theme={navMainStyle}> */}
+            <Link icon='trending_up' href='/#' className={navMainStyle.customLink} theme={navMainStyle} active={this.linkActive('/')}/>
+            <Link icon='view_headline' href='/#/news' className={navMainStyle.customLink} theme={navMainStyle} active={this.linkActive('/news')}/>
+            {/* </Navigation> */}
           </AppBar>
 
         </Panel>
--- app/containers/App.js
+++ app/containers/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router'
 import NavMain from '../components/NavMain';
 import ViewSummary from '../components/ViewSummary';
+import NewsListContainer from '../containers/NewsListContainer'
 import { hashHistory } from 'react-router'
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
       <Router history={hashHistory}>
         <Route path="/" component={NavMain}>
           <IndexRoute component={ViewSummary} />
+          <Route path="news" component={NewsListContainer} />
         </Route>
       </Router>
     )
