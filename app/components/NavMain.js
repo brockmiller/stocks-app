ab856f19630b2872ffe7ccf2956c6b4a15ffb654
Brock Miller <brock@rolltape.com>
2016-08-22 19:07:21 -0400

    add navDrawer and nav buttons.

--- app/components/NavMain.js
+++ app/components/NavMain.js
@@ -1,32 +1,42 @@
 // React
 import React from 'react';
-import { Layout, Navigation, Panel, AppBar, Link } from 'react-toolbox';
-import { TouchIconButton } from './TouchButton';
+import { Layout, Navigation, NavDrawer, Panel, AppBar, Button, IconButton, Avatar } from 'react-toolbox';
 import navMainStyle from '../styles/nav_main.scss';
 
 class NavMain extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-
+    this.state = {
+      navDrawerOpen: false
+    };
+    this.toggleDrawer = this.toggleDrawer.bind(this);
   }
 
-  linkActive(path) {
-    return this.props.location.pathname === path
+  toggleDrawer() {
+    this.setState({ navDrawerOpen: !this.state.navDrawerOpen })
   }
 
   render() {
     return (
-      <Layout>
-        <Panel theme={navMainStyle}>
-          {this.props.children}
+      <Layout theme={navMainStyle}>
+        <NavDrawer theme={navMainStyle} active={this.state.navDrawerOpen} onOverlayClick={this.toggleDrawer}>
+          <div className={navMainStyle.header}>
+            STOCKS
+          </div>
 
-          <AppBar flat theme={navMainStyle}>
-            {/* <Navigation type='horizontal' theme={navMainStyle}> */}
-            <Link icon='trending_up' href='/#' className={navMainStyle.customLink} theme={navMainStyle} active={this.linkActive('/')}/>
-            <Link icon='view_headline' href='/#/news' className={navMainStyle.customLink} theme={navMainStyle} active={this.linkActive('/news')}/>
-            {/* </Navigation> */}
+          <Navigation type='vertical'>
+            <Button theme={navMainStyle} ripple icon='trending_up' label='Watchlist' href='/#' />
+            <Button theme={navMainStyle} ripple icon='view_headline' label='Headlines' href='/#/news' />
+          </Navigation>
+        </NavDrawer>
+
+        <Panel>
+          <AppBar theme={navMainStyle}>
+            <IconButton ripple inverse icon="menu" onClick={this.toggleDrawer}></IconButton>
           </AppBar>
+
+          {this.props.children}
 
         </Panel>
       </Layout>
